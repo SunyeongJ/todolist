6d85770474ce1e2ae15fbdb5da92861c6e594698
MiareJ <psdsd@naver.com>
2021-09-09 03:59:11 +0900

    add youtube audio toggle function

--- music.js
+++ music.js
@@ -1,37 +1,50 @@
-// 2. This code loads the IFrame Player API code asynchronously.
+// 1. This code loads the IFrame Player API code asynchronously.
+
+const VIDEO_ID = "yMZ0je45cqg";
+const playerBtn = document.querySelector("#playerBtn");
+const stopBtn = document.querySelector("#stopBtn");
+let playerStatus = false;
+
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
+// 2. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
+
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-    height: '360',
-    width: '640',
-    videoId: 'yMZ0je45cqg'
+    height: '0',
+    width: '0',
+    videoId: VIDEO_ID,
   });
+};
+
+function toggleAudio() {
+  if (playerStatus === false) {
+    player.playVideo();
+    playerStatus = true;
+    playerBtn.innerHTML = "⏸";
+    console.log(playerStatus);
+  } else {
+    player.pauseVideo();
+    playerStatus = false;
+    playerBtn.innerHTML = "▶";
+    console.log(playerStatus);
+  } 
+};
+
+function stopAudio() {
+  player.stopVideo();
+  playerStatus = false;
+  playerBtn.innerHTML = "▶";
 }
 
-// 4. The API will call this function when the video player is ready.
-
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-function onclickPause() {
-  player.pauseVideo();
-}
+playerBtn.addEventListener("click", toggleAudio);
+stopBtn.addEventListener("click", stopAudio);
 
   // video id
   // yMZ0je45cqg
